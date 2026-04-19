5f26963bbffeeacff598eb4e7dff56ea71b2a93c
castro <105855412+43castro@users.noreply.github.com>
2026-04-19 23:12:55 +0200

    Reconstructed site without bloatware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://castro.eus",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'catppuccin-latte',
@@ -16,6 +15,8 @@ export default defineConfig({
     locales: ["es", "en", "eu"],
   },
   output: "static",
-  prefetch: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "hover",
+  },
 });
-
